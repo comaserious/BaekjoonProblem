0c6a1d1cfdbf087dc73d4bb524f19c540060d5aa
PRO <sciboviator@gmail.com>
2024-03-03 17:47:50 +0900

    baekjoon problem1

---
+++ honey/src/main/java/HoneySize.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HoneySize {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(br.readLine());
+
+
+        int opener = 1;
+        int closer = 2;
+        int layer =1;
+        while(true){
+
+            if(x>=opener && x<closer){
+                System.out.println(layer);
+                break;
+            }
+
+            opener = closer;
+            closer = opener + 6*layer;
+            ++layer;
+
+        }
+
+
+
+    }
+}
